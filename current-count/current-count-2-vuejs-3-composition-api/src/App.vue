68de7e6060f4fb9f9020527a0a56d9d50732c739
Natthaphumin <87896383+Natthaphumin@users.noreply.github.com>
2023-09-15 08:57:55 +0700

    the composition api current count vue.js 3

--- current-count/current-count-2-vuejs-3-composition-api/src/App.vue
+++ current-count/current-count-2-vuejs-3-composition-api/src/App.vue
@@ -1,7 +1,39 @@
 <template>
-  <div></div>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractFromCount()">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
-<style scoped></style>
+const count = ref(10);
+
+const addToCount = () => {
+  console.log("Add to count");
+  count.value = count.value + 1;
+};
+const subtractFromCount = () => {
+  console.log("subtract From count");
+  count.value = count.value - 1;
+};
+</script>
+
+<style scoped>
+main {
+  height: 100vh;
+  max-width: 100vw;
+  background-color: aliceblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+div {
+  text-align: center;
+}
+</style>
